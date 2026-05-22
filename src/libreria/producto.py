ee8e1bc7a6b6f534dbe9c076e9fbec4f7a2314b3
Maria Victoria <mgiraldo3163@cue.edu.co>
2026-05-22 18:02:50 -0500

    GREEN Regla 2: implementar aplicación de descuento

--- src/libreria/producto.py
+++ src/libreria/producto.py
@@ -2,8 +2,15 @@ class Producto:
     def __init__(self, nombre: str, precio_base: float):
         self.nombre = nombre
         self.precio_base = self._validar_precio_base(precio_base)
+        self.descuento = 0
 
     def _validar_precio_base(self, precio_base: float) -> float:
         if precio_base <= 0:
             raise ValueError("El precio base debe ser mayor que cero")
         return precio_base
+
+    def aplicar_descuento(self, descuento: float):
+        if descuento < 0 or descuento > 40:
+            raise ValueError("El descuento debe estar entre 0% y 40%")
+
+        self.descuento = descuento
